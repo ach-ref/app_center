cc8091a04f9016ffa83d7644aef3ebe2ffb25b92
ach-ref <marzoukiachref@hotmail.com>
2021-06-10 16:52:53 +1000

    update post build script - set the iphone simulator sdk version

--- ios/appcenter-post-build.sh
+++ ios/appcenter-post-build.sh
@@ -19,7 +19,7 @@ cd ios
 rm -fr DerivedData
 
 # build for testing
-xcodebuild build-for-testing -sdk iphonesimulator -workspace Runner.xcworkspace -scheme Runner -config Debug -derivedDataPath DerivedData ARCHS="x86_64" VALID_ARCHS="x86_64" 
+xcodebuild build-for-testing -sdk "iphonesimulator14.4" -workspace Runner.xcworkspace -scheme Runner -config Debug -derivedDataPath DerivedData ARCHS="x86_64" VALID_ARCHS="x86_64" 
 
 # test
-xcodebuild test-without-building -xctestrun DerivedData/Build/Products/Runner_iphonesimulator14.5-x86_64.xctestrun -destination 'platform=iOS Simulator,name=iPhone 12 Pro Max'
+xcodebuild test-without-building -xctestrun DerivedData/Build/Products/Runner_iphonesimulator14.4-x86_64.xctestrun -destination 'platform=iOS Simulator,name=iPhone 12 Pro Max'

b003a7f38abebfdd680919b9f28476b39ed6674e
ach-ref <marzoukiachref@hotmail.com>
2021-06-17 17:03:16 +1000

    gcloud first thing in post clone

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -6,7 +6,17 @@ set -e
 # debug log
 set -x
 
-cd ..
+echo $SERVICE_ACCOUNT
+cd $APPCENTER_SOURCE_DIRECTORY
+curl -o google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-345.0.0-darwin-x86_64.tar.gz
+# unpack
+tar -xf google-cloud-sdk.tar.gz
+# auth
+echo $SERVICE_ACCOUNT > /tmp/$CI_PIPELINE_ID.json
+./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
+
+
+cd $APPCENTER_SOURCE_DIRECTORY
 git clone -b beta https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
@@ -14,13 +24,5 @@ flutter channel stable
 flutter doctor
 
 echo "Installed flutter to `pwd`/flutter"
-
-cd $APPCENTER_SOURCE_DIRECTORY
-curl -o google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-345.0.0-darwin-x86_64.tar.gz
-# unpack
-tar -xf google-cloud-sdk.tar.gz
-# auth
-echo "$SERVICE_ACCOUNT" > /tmp/$CI_PIPELINE_ID.json
-./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
 
 flutter build ios --release --no-codesign

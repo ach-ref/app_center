e86c90b3706ac15e34d36bdda0dd201ab139991b
ach-ref <marzoukiachref@hotmail.com>
2021-06-17 14:10:11 +1000

    set iphone sdk to 14.4

--- ios/appcenter-post-build.sh
+++ ios/appcenter-post-build.sh
@@ -8,7 +8,7 @@ set -x
 # vars
 DERIVED_DATA="DerivedData"
 PRODUCT="DerivedData/Build/Products"
-DEV_TARGET="14.5"
+DEV_TARGET="14.4"
 
 # re-export flutter PATH
 cd ..
@@ -40,6 +40,8 @@ zip -r ios_tests.zip Release-iphoneos Runner_iphoneos$DEV_TARGET-arm64.xctestrun
 ######################### google cloud testing
 
 # download google cloud sdk
+echo "/////////////////////// $APPCENTER_SOURCE_DIRECTORY"
+cd $APPCENTER_SOURCE_DIRECTORY
 curl -o google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-345.0.0-darwin-x86_64.tar.gz
 # unpack
 tar -xf google-cloud-sdk.tar.gz
@@ -51,7 +53,7 @@ echo $SERVICE_ACCOUNT > /tmp/$CI_PIPELINE_ID.json
 # make sure the tool results api is enabled
 ./google-cloud-sdk/bin/gcloud services enable toolresults.googleapis.com
 # upload and run the test
-./google-cloud-sdk/bin/gcloud firebase test ios run --test $PRODUCT/ios_tests.zip --device model=iphone11pro,version=14.1,locale=fr_FR,orientation=portrait
+./google-cloud-sdk/bin/gcloud firebase test ios run --test ios/$PRODUCT/ios_tests.zip --device model=iphone11pro,version=14.1,locale=fr_FR,orientation=portrait
 
 
 ######################### cleaning

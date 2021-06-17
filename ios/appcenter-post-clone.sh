601f1725f74c5bdedaeeb8ca0fbf68c24a5532a2
ach-ref <marzoukiachref@hotmail.com>
2021-06-17 17:23:00 +1000

    use sed command

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -6,13 +6,12 @@ set -e
 # debug log
 set -x
 
-echo "$SERVICE_ACCOUNT"
 cd $APPCENTER_SOURCE_DIRECTORY
 curl -o google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-345.0.0-darwin-x86_64.tar.gz
 # unpack
 tar -xf google-cloud-sdk.tar.gz
 # auth
-echo $SERVICE_ACCOUNT > /tmp/$CI_PIPELINE_ID.json
+cat $SERVICE_ACCOUNT | sed -e 's/\\\"/\"/g' -e 's/^.//g' -e 's/.$//g' > /tmp/$CI_PIPELINE_ID.json
 ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
 
 

419e693cad64d5f4061263a524c884caae4e1611
ach-ref <marzoukiachref@hotmail.com>
2021-06-17 16:54:15 +1000

    gcloud in post clone

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -15,4 +15,12 @@ flutter doctor
 
 echo "Installed flutter to `pwd`/flutter"
 
+cd $APPCENTER_SOURCE_DIRECTORY
+curl -o google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-345.0.0-darwin-x86_64.tar.gz
+# unpack
+tar -xf google-cloud-sdk.tar.gz
+# auth
+echo "$SERVICE_ACCOUNT" > /tmp/$CI_PIPELINE_ID.json
+./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
+
 flutter build ios --release --no-codesign
